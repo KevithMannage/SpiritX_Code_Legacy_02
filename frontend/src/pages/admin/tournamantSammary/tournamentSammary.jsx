fb63903b133045a826f19f95d9770bad6c8991c3
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-03-08 18:32:51 +0530

    tournament sammary

---
+++ frontend/src/pages/admin/tournamantSammary/tournamentSammary.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function tournamentSammary() {
+  return (
+    <div>
+      <p>this is admin tournamentSammary</p>
+    </div>
+  )
+}
+
+export default tournamentSammary
