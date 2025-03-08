76d75d10fc043fd3ffa9a36df3e6bf84357e3ee3
kevith mannage <m.mannage@gmail.com>
2025-03-09 00:28:21 +0530

    user,admin login

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -7,27 +7,30 @@ export const createUser = async (username, password) => {
 };
 
 
-export const getUserByEmail = (username) => {
-  return new Promise((resolve, reject) => {
-    connectDB.query(
-      'SELECT * FROM users WHERE username = ? ',
-      [username],
-      (err, results) => {
-        if (err) {
-          console.error('Error querying database:', err);
-          reject(err);  
-        } else {
-          if (results.length > 0) {
-            console.log('User found:', results[0]);
-            resolve(results[0]);  
-          } else {
-            resolve(null);  
-          }
-        }
-      }
+export const getUserByUsername = async (username) => {
+  try {
+    console.log(username);
+
+    // Use await with a promise-based query method
+    const [results] = await connectDB.execute(
+      'SELECT * FROM user WHERE Username = ?',
+      [username]
     );
-  });
+
+    console.log(results);
+
+    if (results.length > 0) {
+      console.log('User found:', results[0]);
+      return results[0]; // Return the user object
+    } else {
+      return null; // Return null if no user is found
+    }
+  } catch (err) {
+    console.error('Error querying database:', err);
+    throw err; // Re-throw the error to handle it in the calling function
+  }
 };
+
 
 // Function to get user by username and password
 export const getUserByEmailAndPassword = (username, password) => {
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import adminRoutes from './routes/adminRoutes.js';
 import { Server } from 'socket.io';
 import http from 'http';
-
+import teamrouters from './Routes/teamroute.js';
 import userroutes from "./Routes/useroute.js";
 import playerroutes from "./Routes/playeroute.js";
 dotenv.config();
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/user", userroutes);
 app.use("/player", playerroutes);
-
+app.use("/team", teamrouters);
 
 
 
