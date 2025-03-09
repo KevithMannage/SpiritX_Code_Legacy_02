2e3f2def46b84af5a4922262071a25a581069e64
kevith mannage <m.mannage@gmail.com>
2025-03-09 20:00:21 +0530

    a

---
+++ spiritx_2_other_data.sql
@@ -0,0 +1,26 @@
+INSERT INTO User (Username, Password, Is_Admin, Created_date, Email)
+VALUES 
+  ('test', '$2b$10$kni2pfFF/0e2JjHOBR8UBeYN24OwfuuIznFng3W.E0SmeEKdIFmnK', 1, NOW(), 'test@gmail.com'),
+  ('codelegacy', '$2b$10$t3q8ZJgZIMasyqaHQPcOwudUw1J9kiuHobwKKNP9NEe0h9OIcUMjS', 1, NOW(), 'codelegacy@gmail.com');
+  
+
+-- INSERT INTO `Team` (`Team_Name`, `Created_Date`, `User_ID`, `Initial_Budget`) VALUES
+
+-- ('Wilson Warriors', '2025-02-11 11:15:00', 3, 120000.00);
+
+
+-- INSERT INTO `Team_Members` (`User_ID`, `Player_ID`, `Added_Date`) VALUES
+-- (1, 1, '2025-01-17 09:00:00'), 
+-- (1, 2, '2025-01-17 09:05:00'), 
+-- (1, 3, '2025-01-17 09:05:00'),
+-- (1, 4, '2025-01-17 09:05:00'),
+-- (1, 5, '2025-01-17 09:05:00'),
+-- (1, 6, '2025-01-17 09:05:00'),
+-- (1, 7, '2025-01-17 09:05:00'),
+-- (1, 8, '2025-01-17 09:05:00'),
+-- (1, 9, '2025-01-17 09:05:00'),
+-- (1, 10, '2025-01-17 09:05:00'),
+-- (1, 11, '2025-01-17 09:05:00');
+
+
+
---
+++ spiritx_2_tables.sql
@@ -0,0 +1,53 @@
+drop database if exists spiritx_2;
+create database spiritx_2;
+use spiritx_2;
+
+CREATE TABLE `User` (
+  `User_ID` int AUTO_INCREMENT,
+  `Username` varchar(100),
+  `Password` varchar(255),
+  `Is_Admin` boolean,
+  `Created_date` datetime default current_timestamp,
+  `Email` varchar(150),
+  PRIMARY KEY (`User_ID`),
+  UNIQUE (`Username`),
+  UNIQUE (`Email`)
+);
+
+-- CREATE TABLE `Team` (
+--   `Team_ID` int AUTO_INCREMENT,
+--   `Team_Name` varchar(100),
+--   `Created_Date` Datetime default current_timestamp,
+--   `User_ID` int,
+--   `Initial_Budget` Decimal(10,2),
+--   PRIMARY KEY (`Team_ID`),
+--   FOREIGN KEY (`User_ID`) REFERENCES `User`(`User_ID`),
+--    unique (`Team_Name`)
+-- );
+
+CREATE TABLE `Player` (
+  `Player_ID` int auto_increment,
+  `Name` varchar(200),
+  `University` varchar(200),
+  `Category` enum('Batsman','All-Rounder','Bowler'),
+  `Total_Runs` int,
+  `Balls_Faced` int,
+  `Innings_Played` int,
+  `Wickets` int,
+  `Overs_Bowled` int,
+  `Runs_Conceded` int,
+  `Created_at` datetime default current_timestamp,
+  `Updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Player_ID`)
+);
+
+CREATE TABLE `Team_Members` (
+  `User_ID` int,
+  `Player_ID` int,
+  `Purchased_Price` int,
+  `Added_Date` Datetime default current_timestamp,
+  PRIMARY KEY (`User_ID`, `Player_ID`),
+  FOREIGN KEY (`User_ID`) REFERENCES `User`(`User_ID`),
+  FOREIGN KEY (`Player_ID`) REFERENCES `Player`(`Player_ID`)
+);
+
